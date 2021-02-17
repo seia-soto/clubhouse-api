f6c42cc0ad218566de125602050b7b88e1fa7cde
Seia-Soto <seia@outlook.kr>
2021-02-17 17:47:21 +0900

    Add pubnub keys

--- src/profiles/application/a304.js
+++ src/profiles/application/a304.js
@@ -3,5 +3,7 @@ export default {
   userAgentStatic: 'Clubhouse/304 CFNetwork/1220.1 Darwin/20.3.0',
   appVersion: '0.1.28',
   appBuild: '304',
-  agoraKey: '938de3e8055e42b281bb8c6f69c21f78'
+  agoraKey: '938de3e8055e42b281bb8c6f69c21f78',
+  pubnubPubKey: 'pub-c-6878d382-5ae6-4494-9099-f930f938868b',
+  pubnubSubKey: 'sub-c-a4abea84-9ca3-11ea-8e71-f2b83ac9263d'
 }
