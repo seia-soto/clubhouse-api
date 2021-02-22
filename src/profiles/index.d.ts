be00e82ddd514e9a0f9f8d0fe924a0926ad8d24f
mrmld <die.marmelade@yandex.ru>
2021-02-22 12:55:42 +0300

    Use cross-fetch instead of node-fetch

--- src/profiles/index.d.ts
+++ src/profiles/index.d.ts
@@ -1,15 +1,23 @@
-type Locale = { languages: string, locale: string, acceptLanguages: string }
+type Locale = { languages?: string, locale?: string, acceptLanguages?: string }
 type Application = {
-  apiRoot: string
-  userAgnet: string
-  userAgentStatic: string
-  appVersion: string
-  appBuild: string
-  agoraKey: string
-  pubnubRoot: string
-  pubnubPubKey: string
-  pubnubSubKey: string
-  pubnubSDK: string
+  apiRoot?: string
+  userAgent?: string
+  userAgentStatic?: string
+  appVersion?: string
+  appBuild?: string
+  agoraKey?: string
+  pubnubRoot?: string
+  pubnubPubKey?: string
+  pubnubSubKey?: string
+  pubnubSDK?: string
+  acceptEncodings?: string
+}
+
+type Profile = Application & Locale & {
+  token?: string,
+  userId?: string,
+  deviceId?: string,
+  fetchOptions?: any
 }
 
 export declare const application: { a297: Application, a304: Application }
